8f89f9ce3966f25d869398598259207038fada56
nelistik <55016104+Nelistik@users.noreply.github.com>
2022-10-23 21:15:15 +0300

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,14 @@
+
+def palindrome (num: int): #функция, принимающая на вход int
+    num = str(num) #преобразование int в строку
+    return num == num[::-1] #возврат T/F после проверки условия (с перевёрнутой строкой)
+
+#реализация работы функции
+print(palindrome(24))
+print(palindrome(123)) 
+print(palindrome(1233321))
+print(palindrome(231124))
+print(palindrome(9888889))
+print(palindrome(445544))
+print(palindrome(98777))
+print(palindrome(121))
