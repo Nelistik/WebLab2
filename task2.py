335bc29477f380e8153cfbb6232af87e0458bfb4
nelistik <55016104+Nelistik@users.noreply.github.com>
2022-10-23 21:57:39 +0300

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,17 @@
+def aliquot(num_list):
+    al_2 = [n for n in num_list
+            if n%2 == 0]
+    al_3 = [n for n in num_list
+            if n%3 == 0]
+    al_4 = [n for n in num_list
+            if n%4 == 0]
+    return al_2, al_3, al_4
+
+
+#реализация работы функции
+num_list = [2,3,4,5,6,22,24,25,9,22,6,9]
+al_2, al_3, al_4 = aliquot(num_list)
+
+print ('a. Делятся на 2:\n', al_2)
+print ('b. Делятся на 3:\n', al_3)
+print ('c. Делятся на 4:\n', al_4)
